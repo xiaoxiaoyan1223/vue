2fd595395637e9a6dd58fb8815920de414bfc312
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2022-12-07 11:34:31 +0800

    Add files via upload

---
+++ _src_todoList/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="root">
+    <div class="todo-container">
+        <div class="todo-wrap">
+            <MyHeader :addTodo='addTodo'/>
+            <MyList :todos='todos' :checkTodo='checkTodo' :deleteTodo='deleteTodo'/>
+            <MyFooter :todos='todos' :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader'
+import MyFooter from './components/MyFooter';
+import MyList from './components/MyList';
+export default {
+  name: 'App',
+  components:{MyHeader,MyList,MyFooter},
+  data(){
+    return {
+      todos:[
+                {id:'001',title:'唱歌',done:false},
+                {id:'002',title:'跳舞',done:true},
+                {id:'003',title:'跑步',done:false},
+                {id:'004',title:'学习',done:true},
+            ]
+    }
+  },
+  methods:{
+    addTodo(todoObj){
+      this.todos.unshift(todoObj)
+    },
+    checkTodo(id){
+      this.todos.forEach((todos)=>{
+        if(todos.id===id) todos.done=!todos.done
+      })
+    },
+    deleteTodo(id){
+     this.todos=this.todos.filter((todo)=>{
+      return todo.id!==id
+      })
+    },
+    checkAllTodo(done){
+      this.todos.forEach((todos)=>{
+        todos.done=done
+      })
+
+    },
+    clearAllTodo(){
+      this.todos=this.todos.filter((todos)=>{
+        return !todos.done
+      })
+    }
+  }
+}
+</script>
+<style>
+body{
+  background: #fff;
+}
+
+.btn{
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255,255,255,0.2),0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger{
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover{
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus{
+  outline: none;
+}
+
+.todo-container{
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap{
+  padding: 10px;
+  border:1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ _src_todoList/components/MyItem.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <li>
+         <label >
+             <input type="checkbox" :checked='todo.done' @change='handleCheck(todo.id)'/>
+             <!-- 如下代码也能实现功能，但是不太推荐，因为有点违反原则，修改了props -->
+             <!-- <input type="checkbox" v-model="todo.done"/> -->
+             <span>{{todo.title}}</span>
+         </label>
+         <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+        </li>
+    </div>
+</template>
+<script>
+export default {
+    name:'MyItem',
+    //通过props将数据从父组件传递到子组件
+    props:['todo','checkTodo','deleteTodo'],
+    methods:{
+    //勾选
+    handleCheck(id){
+        this.checkTodo(id)
+    },
+    //删除
+    handleDelete(id){
+        if(confirm('确认删除吗')){
+            this.deleteTodo(id)
+        }
+    }
+    }
+}
+</script>
+<style scoped>
+li{
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+}
+li label{
+    float:left;
+    cursor: pointer;
+}
+
+li label li input{
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+}
+li button{
+    float: right;
+    display: none;
+    margin-top: 3px;
+}
+
+li:before{
+    content: initial;
+}
+li:last-child{
+    border-bottom: none;
+}
+li:hover {
+    background-color: #ddd;
+}
+li:hover button {
+    display: block;
+}
+</style>
