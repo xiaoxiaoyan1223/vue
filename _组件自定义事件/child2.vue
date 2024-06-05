5fb5dd712cbbeb66fc58accdc3a7f3730c58d984
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-06-05 11:28:56 +0800

    Update child2.vue

--- _组件自定义事件/child2.vue
+++ _组件自定义事件/child2.vue
@@ -1,8 +1,19 @@
 <template>
     <button @click="sentChild2Name"></button>
+    <input type="text" 
+          :value="text" 
+          @inout=($emit("change1",$event.target.value))
 </template>
 <script>
 export default{
+    model:{
+        props:text,
+        event:change1
+    }
+    props:{
+    text1:String
+    }
+    
     name:'Child2',
     methods:{
         sentChild2Name(){
